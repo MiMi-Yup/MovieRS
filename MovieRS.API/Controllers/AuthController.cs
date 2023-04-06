d34d09f355374878974c400e5603d9bcdf783a5f
MiMi-Yup <tuivanthiet@gmail.com>
2023-04-07 01:31:26 +0700

    finaly

--- MovieRS.API/Controllers/AuthController.cs
+++ MovieRS.API/Controllers/AuthController.cs
@@ -106,12 +106,5 @@ namespace MovieRS.API.Controllers
         {
             return new Random().Next(1000, 9999).ToString("D4");
         }
-
-        [HttpGet]
-        [Route("test")]
-        public IActionResult Test()
-        {
-            return Ok(Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory));
-        }
     }
 }
